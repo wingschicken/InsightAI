67e3670ffe35410c66d22f9a59fe4bbbf58f6847
wingschicken <63052845+wingschicken@users.noreply.github.com>
2026-04-17 20:35:07 +0200

    version_2: added the vektor imbeddings with cve/description into seperate container

--- rag/embedder.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def embed(texts):
-    return model.encode(texts, batch_size=32).tolist()
--- rag/utils.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-
-def generate_id(text: str) -> int:
-    return int(hashlib.sha256(text.encode()).hexdigest(), 16) % (10**12)
