64162acbff2aaacd42c4dfd7cfc7015826180035
wingschicken <63052845+wingschicken@users.noreply.github.com>
2026-04-17 20:41:23 +0200

    version_2: added the vektor imbeddings with cve/description into seperate container

--- app/main.py
+++ app/main.py
@@ -354,5 +354,5 @@ def favicon():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("APP_PORT", os.environ.get("PORT", "8000")))
+    port = int(os.environ.get("PORT", "8000")))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
